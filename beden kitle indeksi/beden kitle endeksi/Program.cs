96dc7a996cd0c70feebfa73f1ba44f0e5b608c24
cerencotukk <cerencotukk@gmail.com>
2022-09-23 14:49:15 +0300

    BEDEN KİTLE İNDEKSİ
    STAJ

---
+++ beden kitle indeksi/beden kitle endeksi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace beden_kitle_endeksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+        
+
+            Console.WriteLine("Kilonuzu Giriniz:" );
+            int kilo= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Boyunuzu Giriniz: ");
+            double  boy = Convert.ToDouble(Console.ReadLine());
+         
+
+            double bke = kilo / (boy * boy);
+
+            if (bke < 18)
+            {
+                Console.WriteLine("ZAYIFSINIZ");
+                Console.ReadLine();
+
+            }
+            else if (bke > 18 && bke < 25)
+            {
+                Console.WriteLine("NORMALSİNİZ");
+                Console.ReadLine();
+            }
+            else if (bke > 25)
+            { 
+                Console.WriteLine("OBEZSİNİZ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
